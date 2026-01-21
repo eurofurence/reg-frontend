7286100744a36134cc52df86e3a0d04f8b97d8da
Jumpy Squirrel <jsquirrel_github_9a6d@packetloss.de>
2026-01-21 20:40:02 +0100

    feat(#300): temp enable

--- src/config.ts
+++ src/config.ts
@@ -1049,7 +1049,7 @@ const configEf = {
     contact: 'https://help.eurofurence.org/contact',
   },
   disablePackageEditForStatuses: ['paid', 'checked-in', 'cancelled'],
-  disableCCPayments: true,
+  disableCCPayments: false,
   disableSEPAPayments: false,
 } as const
 

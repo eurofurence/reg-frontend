c30e26a0837933aa58305dacd8c701fd66dd439f
Jumpy Squirrel <jsquirrel_github_9a6d@packetloss.de>
2026-01-26 19:42:11 +0100

    fix(#307): load registration for payment landing pages for correct locale

--- src/pages/payment-failure.tsx
+++ src/pages/payment-failure.tsx
@@ -2,12 +2,25 @@ import Layout from '~/components/layout'
 import SEO from '~/components/seo'
 import type { ReadonlyRouteComponentProps } from '~/util/readonly-types'
 import Failure from '~/components/funnels/funnels/payment/failure'
+import { useAppDispatch } from '~/hooks/redux'
+import { useEffect } from 'react'
+import { CheckCountdown } from '~/state/actions/register'
 
 export const Head = () => <SEO title="Payment Failure" />
 
+const Content = () => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(CheckCountdown.create(undefined))
+  }, [])
+
+  return <Failure />
+}
+
 const PaymentFailurePage = (_: ReadonlyRouteComponentProps) => (
   <Layout>
-    <Failure />
+    <Content />
   </Layout>
 )
 
